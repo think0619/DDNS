292ae9129435e6439d898fab9885c43665fe702c
Hsuing <thinkxs@outlook.com>
2023-03-28 17:23:41 +0800

    finish

---
+++ Code/main.py
@@ -0,0 +1,86 @@
+import time
+import requests
+import json
+import urllib.request 
+from urllib.request import Request, urlopen
+import urllib.error
+from threading import Timer
+
+DNSPodLoginToken='Login_Token' #'123456,qazwsxedcrfv'
+DominId='DominId' #'123456'
+RecordId='RecordId' #'123456'
+
+
+def getLocalIPV4():   
+# http://ip.42.pl/raw
+# https://ifconfig.me/ip
+  getIPResponse = requests.request("GET", 'https://ifconfig.me/ip', headers={}, data={},timeout=10)
+  if(getIPResponse.status_code==200):  
+   return getIPResponse.text
+ 
+def getLocalIPV4_01(): 
+   # http://jsonip.com
+   # https://api.ipify.org?format=json
+   getIPResponse = requests.request("GET", 'https://api.ipify.org?format=json', headers={}, data={},timeout=20)
+   if(getIPResponse.status_code==200):  
+    #print(getIPResponse.text)
+    responseObj = json.loads(getIPResponse.text)
+    ipstr=responseObj["ip"]
+    return ipstr
+  
+def getDomainRecords():
+   url="https://dnsapi.cn/Record.List"
+   data={
+    'login_token': DNSPodLoginToken,
+    'format': 'json',
+    'domain_id': DominId,
+    }
+   getDomainResponse = requests.request("POST", url, headers={}, data=data,timeout=10)
+   if(getDomainResponse.status_code==200):  
+    print(getDomainResponse.text) 
+
+
+def updateDNSRecord(newip):
+  #doc: https://docs.dnspod.cn/api/update-dns-records/
+  #login_token : need signup and get token
+  #domain_id：
+  #record_id：
+  #sub_domain
+  url="https://dnsapi.cn/Record.Ddns"
+  data={
+    'login_token': '398390,f244936090331d24d4e746bfc74ad8ab',
+    'format': 'json',
+    'domain_id': DominId,
+    'record_id': RecordId,
+    'record_line': '默认',
+    'record_line_id': '0',
+    'value': newip,
+    'sub_domain': 'rasp'
+  }
+  updateDNSResponse = requests.request("POST", url, headers={}, data=data,timeout=10)
+  if(updateDNSResponse.status_code==200):  
+    #print(updateDNSResponse.text) 
+    responseObj = json.loads(updateDNSResponse.text)
+    respStatus=responseObj["status"]["code"]
+    if(respStatus=='1'):
+      return 'success'
+    else:
+      return 'fail'
+
+  
+#getDomainRecords()
+
+def GetIPv4UpdateDNSRecord():
+  print('s')
+  currentIP=getLocalIPV4_01()
+  print(updateDNSRecord(currentIP))
+
+#update dns every 120s once
+GetIPv4UpdateDNSRecord()
+sTimer = Timer(120, GetIPv4UpdateDNSRecord)
+sTimer.start()
+
+
+
+
+      
--- README.md
+++ README.md
@@ -6,3 +6,13 @@
 > 1. 获取本地公网IP 暂时借助 `https://api.ipify.org?format=json` .想到自己几年前薅阿里云羊毛，写过相关服务，但是服务器续费太贵，试了下这个可以用但是速度比较慢，暂时先凑合吧。淘宝的API已经不能使用了。
 > 2. 将获取到的信息通过DNSPod接口 `https://dnsapi.cn/Record.Ddns`  更新
 > 3. 以上步骤开个定时器运行即可。
+
+
+
+找了几个可用的获取本地IPV4公网地址的接口
+
+- http://ip.42.pl/raw
+- https://ifconfig.me/ip
+
+- http://jsonip.com
+- https://api.ipify.org?format=json
